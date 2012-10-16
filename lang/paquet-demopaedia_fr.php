4adc9eb3e9384b492b6f1241383b92caff34f250
Joseph Larmarange <joseph@larmarange.net>
2012-10-16 20:53:02 +0200

    Préparation au passage à SPIP 3

---
+++ lang/paquet-demopaedia_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-demopaedia
+// Langue: fr
+// Date: 16-10-2012 18:51:10
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// D
+	'demopaedia_description' => 'Ce plugin permet de générer différents documents à partir des dictionnaires démographiques multilingues.',
+	'demopaedia_slogan' => 'Ce plugin permet de générer différents documents à partir des dictionnaires démographiques multilingues',
+);
+?>
